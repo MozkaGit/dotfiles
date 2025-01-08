ffe052aa0154371988c28051460c7ba665107609
MozkaGit <mozkaops@gmail.com>
2025-01-08 19:46:12 +0100

    update idea.fish

---
+++ fish/.config/fish/functions/idea.fish
@@ -0,0 +1,4 @@
+function idea --wraps=/home/mozka/Developer/idea-intellij/bin/idea.sh --description 'alias idea=/home/mozka/Developer/idea-intellij/bin/idea.sh'
+  /home/mozka/Developer/idea-intellij/bin/idea.sh $argv
+        
+end
