1ad14f676580a01029344e9293a718dd6a3e6965
MozkaGit <mozkaops@gmail.com>
2025-05-22 15:21:31 +0800

    refactor!: remove legacy Stow-based configuration files
    BREAKING CHANGE: Migrated old dotfiles structure to Chezmoi
    
    - Remove fish/, ghostty/, nushell/, nvim/, solaar/, starship/, tmux/,
    vim/, wezterm/ directories
    - Legacy Stow symlink structure replaced by Chezmoi managed files
    - All configurations has been migrated to new dot_config/ structure

--- fish/.config/fish/completions/asdf.fish
+++
@@ -1,1 +0,0 @@
-/home/mozka/.asdf/completions/asdf.fish
--- fish/.config/fish/functions/idea.fish
+++
@@ -1,4 +0,0 @@
-function idea --wraps=/home/mozka/Developer/idea-intellij/bin/idea.sh --description 'alias idea=/home/mozka/Developer/idea-intellij/bin/idea.sh'
-  /home/mozka/Developer/idea-intellij/bin/idea.sh $argv
-        
-end
