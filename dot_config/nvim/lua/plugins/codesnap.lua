004f89e6ab8e51799dc7284873ab94730524ebf0
MozkaGit <mozkaops@gmail.com>
2025-05-25 09:16:07 +0000

    feat(nvim): add CodeSnap.nvim to make pretty code snapshots for Neovim

---
+++ dot_config/nvim/lua/plugins/codesnap.lua
@@ -0,0 +1,24 @@
+return {
+	"mistricky/codesnap.nvim",
+	build = {
+		"make",
+	},
+	config = function()
+		require("codesnap").setup({
+			mac_window_bar = true,
+			title = "CodeSnap.nvim",
+			code_font_family = "CaskaydiaCove Nerd Font",
+			watermark_font_family = "Pacifico",
+			watermark = "",
+			bg_theme = "summer",
+			breadcrumbs_separator = "/",
+			has_breadcrumbs = true,
+			has_line_number = true,
+			show_workspace = true,
+			min_width = 0,
+			bg_x_padding = 122,
+			bg_y_padding = 82,
+			save_path = os.getenv("XDG_PICTURES_DIR") or os.getenv("PWD") or (os.getenv("HOME") .. "/Pictures"),
+		})
+	end,
+}
