454f4162ecd026b0858ba18add8cdb13adb35a61
MozkaGit <mozkaops@gmail.com>
2025-05-26 03:29:19 +0000

    feat(nvim,tmux): add OSC 52 clipboard support for remote environments
    Add clipboard functionality for DevPod/SSH environments without display server by configuring OSC 52 support in both tmux and Neovim for universal clipboard access.

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -1,3 +1,18 @@
 vim.opt.ignorecase = false
 
 vim.opt.scrolloff = 8
+
+-- Enable OSC 52 clipboard for remote environments
+if os.getenv("SSH_TTY") or os.getenv("DEVPOD") then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
