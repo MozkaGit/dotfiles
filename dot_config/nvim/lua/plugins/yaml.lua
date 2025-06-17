2f2602bd51cba5e79fc37daf31709a8aba30fc80
MozkaGit <mozkaops@gmail.com>
2025-06-17 00:25:37 +0000

    feat: add yaml-jumper to quickly navigate through a yaml file

--- dot_config/nvim/lua/plugins/yaml.lua
+++ dot_config/nvim/lua/plugins/yaml.lua
@@ -124,4 +124,16 @@ return {
 			return opts
 		end,
 	},
+
+	{
+		"DmarshalTU/yaml-jumper",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("yaml-jumper").setup()
+		end,
+		ft = { "yaml", "yml" },
+	},
 }
